f8c2a0db7f4aea068203d08785cb7be42c96b3b0
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2019-12-21 20:16:33 +0300

    Project "House" is created

---
+++ House/House/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  House
+//
+//  Created by Artyom Sadyrin on 12/21/19.
+//  Copyright © 2019 Artyom Sadyrin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
